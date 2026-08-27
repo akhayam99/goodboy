121cdc1a236c95b607e9e07797a8701b86a712e3
Dev <demo@example.com>
2026-08-27 16:28:42 +0200

    chore(repo): bump version to 0.2.1

--- website/src/site.ts
+++ website/src/site.ts
@@ -1,5 +1,5 @@
 export const SITE = {
-  version: 'v0.2.0',
+  version: 'v0.2.1',
   repo: 'https://github.com/akhayam99/goodboy',
   releases: 'https://github.com/akhayam99/goodboy/releases',
   latest: 'https://github.com/akhayam99/goodboy/releases/latest',
